98ff31d4cb14a1c52b3fc09e645654215d527503
Dan <dpedersen84@gmail.com>
2018-04-21 18:30:17 -0500

    functions created in liri.js

--- liri.js
+++ liri.js
@@ -1,7 +1,46 @@
 let env = require("dotenv").config();
-let keys = require("./keys.js")
+let keys = require("./keys.js");
+let request = require("request");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-console.log("hello");
+let command = process.argv[2];
+
+// Switch statement for command
+
+switch (command) {
+    case "my-tweets":
+    myTweets();
+    break;
+
+    case "spotify-this-song":
+    spotifySong();
+    break;
+
+    case "movie-this":
+    movieThis();
+    break;
+
+    case "do-what-it-says":
+    doWhatItSays();
+    break;
+}
+
+//Functions
+
+function myTweets() {
+
+};
+
+function spotifySong() {
+
+};
+
+function movieThis() {
+
+};
+
+function doWhatItSays() {
+
+};
