882ba660bbe042aff818dce9c567729202e41df0
Dan <dpedersen84@gmail.com>
2018-04-21 10:00:30 -0500

    deleted keys

--- keys.js
+++ keys.js
@@ -1,14 +1,14 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.JDXAeGzoqc0NMeTfvpmPyJH4H,
-  consumer_secret: process.env.Du7GZwx133aIzJquK528Q0rDAV1YPjglNHxgEu9Y1pEwfAN3mF,
-  access_token_key: process.env.PeUAJG8Sq1qzUOiTLOXq82pU8eqli5U,
-  access_token_secret: process.env.BXryEzIySKqJJwIvJPJV2S7xUPzDCnDVRb0fZjlvknUHD
-
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: process.env.cc31a699fcee4cd7be2a1e8c6a1526ae,
-  secret: process.env.fc01276dd6684c438ea33ad83932cfb8
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
+
