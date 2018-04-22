923b363bc52eb6a84e8dc3b9c9407c93a471076e
Dan <dpedersen84@gmail.com>
2018-04-21 19:09:09 -0500

    movieThis function

--- liri.js
+++ liri.js
@@ -1,6 +1,8 @@
 let env = require("dotenv").config();
 let keys = require("./keys.js");
 let request = require("request");
+let Spotify = require("node-spotify-api");
+let Twitter = require("twitter");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
@@ -30,17 +32,35 @@ switch (command) {
 //Functions
 
 function myTweets() {
-
+    console.log("my tweets");
 };
 
 function spotifySong() {
-
+    console.log("my spotify");
 };
 
 function movieThis() {
+    
+    let movieName = process.argv[3];
+
+    let queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    console.log(queryURL);
+
+    request(queryURL, function(error, response, body) {
+
+        console.log("Title: " + JSON.parse(body).Title);
+        console.log("Year Released: " + JSON.parse(body).Year);
+        console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+        console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
+        console.log("Country Produced: " + JSON.parse(body).Country);
+        console.log("Language: " + JSON.parse(body).Language);
+        console.log("Plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
+    })
 
 };
 
 function doWhatItSays() {
-
+    console.log("my do what it says");
 };
